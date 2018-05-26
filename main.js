072ae5dbfa9cb5600fe9c4d8a5c2f39928a16332
Nate Denys <nate_denys@yahoo.com>
2018-05-26 10:20:42 -0700

    create first method and computed properties
    listen for v-on:click to add votes, compute sorted submissions list

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ new Vue({
     el: '#app',
     data: {
         submissions: Seed.submissions
+    },
+    // first computed property
+    computed: {
+        sortedSubmissions() {
+            // return a sorted list of submmisions from most to least
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes
+            });
+        }
+    },
+    // first method
+    methods: {
+        // retrieved v-on event listener and increment vote count by 1
+        upvote(submissionId) {
+            const submission = this.submissions.find(submission => submission.id ===submissionId);
+            submission.votes++;
+        }
     }
 });
